97aeba99f142f548b5c003eda7cc56e0fb58673a
Caeldeth <sabrael@hybrasyl.com>
2021-09-23 18:05:36 -0500

    minor css tweak, begin work on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-// Get document elements from index
+// Get document elements from index and assign to variables
+var task9 = $("#hour-9");
+var task10 = $("#hour-10");
+var task11 = $("#hour-11");
+var task12 = $("#hour-12");
+var task13 = $("#hour-13");
+var task14 = $("#hour-14");
+var task15 = $("#hour-15");
+var task16 = $("#hour-16");
+var task17 = $("#hour-17");
 
-// create form
+// get current date
+var dayIs = moment().format("dddd, MMMM Do, YYYY");
+
+// create function to start app
+function beginApp() {
+
+}
